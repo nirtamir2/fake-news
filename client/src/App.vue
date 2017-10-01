641ab563e265e12cba11d62f0763e0a0b637f611
Nir Tamir <nirtamir2@gmail.com>
2017-10-01 19:20:12 +0300

    add search component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
-    </main>
+    <v-app>
+      <v-toolbar fixed class="red" dark>
+        <v-toolbar-title>Fake News</v-toolbar-title>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </main>
+      <v-footer></v-footer>
+    </v-app>
   </div>
 </template>
 
@@ -15,40 +19,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-</style>
---
+++ client/src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form action="submit" @submit.prevent="searcha">
+    <v-text-field
+     solo clearable
+      label="Search" prepend-icon="search"
+       v-model="searchParams" >
+    </v-text-field>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchParams: ''
+    }
+  },
+  methods: {
+    searcha () {
+      console.log('searching', this.searchParams)
+    }
+  }
+}
+</script>
+
