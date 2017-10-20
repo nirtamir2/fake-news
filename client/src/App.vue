4d8f8fa3db0d513078944b66c580f2df83ec4374
Nir Tamir <nirtamir2@gmail.com>
2017-10-20 09:29:09 +0300

    add nevigation buttons to search components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <v-app>
-      <v-toolbar fixed class="red" dark>
+      <v-toolbar fixed dark class="red">
         <v-toolbar-title>Fake News</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn flat to='/'>Title</v-btn>
+          <v-btn flat to='/domain'>URL</v-btn>
+        </v-toolbar-items>
       </v-toolbar>
       <main>
         <v-container fluid>
